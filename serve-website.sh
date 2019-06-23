6b3a54b20da6fa95f947fa3d0849ebc5e1d28fa0
Chris Kempson <git@chriskempson.com>
2019-06-23 22:18:12 +0100

    Updates serve script

---
+++ serve-website.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# 
+# serve-website.sh - chriskempson.com
+#
+# Fires up PHP-CLI's in-build webserver and dishes out the files within 
+# 'public'. Depening on your Terminal you can Ctrl+Click the printed URL to 
+# open it up in your default browser.
+
+printf "\n---- Point your browser at http://127.0.0.1:8000\n\n" 
+php -S 0.0.0.0:8000 -t public
